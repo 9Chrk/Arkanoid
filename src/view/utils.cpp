e78a3b0350cb8d2bbb850f5601bc7b3d2265cbd3
Rayan <rayan-yasmine@hotmail.com>
2025-08-16 15:08:03 +0200

    modification getcolor

--- src/view/utils.cpp
+++ src/view/utils.cpp
@@ -1,47 +1,44 @@
-#include <stdexcept>
-#include <string>
+using namespace std;
+#include <unordered_map>
 #include "view/utils.hpp"
 
 
-// Allegro error handling 
+static const unordered_map<string, ALLEGRO_COLOR> COLORS{
+        {"WHITE", WHITE},   {"GREY", GREY},     {"BLACK", BLACK},
+        {"ORANGE", ORANGE}, {"CYAN", CYAN},     {"GREEN", GREEN},
+        {"RED", RED},       {"BLUE", BLUE},     {"MAGENTA", MAGENTA},
+        {"YELLOW", YELLOW}, {"SILVER", SILVER}, {"GOLD", GOLD}};
 
-void checkBitmap(const ALLEGRO_BITMAP* bitmap, const std::string& fileName) {
-  if (!bitmap) throw std::runtime_error("Failed to load image: " + fileName);
+// Allegro error handling
+
+void checkBitmap(const ALLEGRO_BITMAP* bitmap, const string& fileName) {
+    if (!bitmap) throw runtime_error("Failed to load image: " + fileName);
 }
 
-void checkSample(const ALLEGRO_SAMPLE* sample, const std::string& fileName) {
-  if (!sample) throw std::runtime_error("Failed to load sound: " + fileName);
+void checkSample(const ALLEGRO_SAMPLE* sample, const string& fileName) {
+    if (!sample) throw runtime_error("Failed to load sound: " + fileName);
 }
 
 // Allegro resource loading
 
-ALLEGRO_BITMAP* loadBitmap(const std::string& filename) {
-  ALLEGRO_BITMAP* bmp = al_load_bitmap(filename.c_str());
-  checkBitmap(bmp, filename);
-  al_convert_bitmap(bmp);
-  return bmp;
+ALLEGRO_BITMAP* loadBitmap(const string& filename) {
+    ALLEGRO_BITMAP* bmp = al_load_bitmap(filename.c_str());
+    checkBitmap(bmp, filename);
+    al_convert_bitmap(bmp);
+    return bmp;
 }
 
-ALLEGRO_SAMPLE* loadSample(const std::string& filename) {
-  ALLEGRO_SAMPLE* sample = al_load_sample(filename.c_str());
-  checkSample(sample, filename);
-  return sample;
+ALLEGRO_SAMPLE* loadSample(const string& filename) {
+    ALLEGRO_SAMPLE* sample = al_load_sample(filename.c_str());
+    checkSample(sample, filename);
+    return sample;
 }
 
 // Color utility function
 
-[[gnu::pure]] ALLEGRO_COLOR getColor(const std::string& colorName) {
-  if      (colorName == "WHITE")   return WHITE;
-  else if (colorName == "GREY")    return GREY;
-  else if (colorName == "BLACK")   return BLACK;
-  else if (colorName == "ORANGE")  return ORANGE;
-  else if (colorName == "CYAN")    return CYAN;
-  else if (colorName == "GREEN")   return GREEN;
-  else if (colorName == "RED")     return RED;
-  else if (colorName == "BLUE")    return BLUE;
-  else if (colorName == "MAGENTA") return MAGENTA;
-  else if (colorName == "YELLOW")  return YELLOW;
-  else if (colorName == "SILVER")  return SILVER;
-  else if (colorName == "GOLD")    return GOLD;
-  else { return BLACK; }
+[[gnu::pure]] ALLEGRO_COLOR getColor(const string& colorName) {
+    if (const auto it = COLORS.find(colorName); it != COLORS.end()) {
+        return it->second;
+    }
+    return BLACK;
 }
