c1e66ec442529aa2f20d3e021f8adf6a441f65bb
Rayan <rayan-yasmine@hotmail.com>
2025-08-28 05:32:10 +0200

    bonus expand

--- src/model/Split.cpp
+++ src/model/Split.cpp
@@ -7,8 +7,4 @@ Split::Split(const Point& position, float w, float h, float fallSpeed)
 Split::~Split() = default;
 
 void Split::applyEffect(GameModel& model) {
-  Ball& ball = model.getBall();
-  Point dir = ball.getDirection();
-  dir.x *= -1.0f; // simple approximation of a split
-  ball.setDirection(dir);
 }
